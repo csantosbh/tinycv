6060b37012a02829667d405cedc9068076daad77
Claudio dos Santos Fernandes <claudius@ufmg.br>
2017-10-22 21:01:25 -0200

    Update experiments with MI space surface plots

--- src/experiments/mutual_information_space.m
+++ src/experiments/mutual_information_space.m
@@ -1,15 +1,19 @@
 close all;
 
 function data=plot_surf(filename, range)
-  data = load(filename);
+  data = load(sprintf('data/%s', filename));
   tx = ty = linspace(-range, range, size(data, 1));
 
   figure;
-  surf(tx, ty, data);
+  surfc(tx, ty, data, -10);
 
   title(filename);
   xlabel('x');
   ylabel('y');
+
+  zmin = 0;
+  zmax = max(data(:));
+  axis([-range range -range range (zmin - 0.5) (zmax + 0.0)]);
 endfunction
 
 
@@ -22,8 +26,8 @@ endfunction
 %plot_surf('perspective_scaled_jitter.txt', 20);
 %plot_surf('perspective_scaled_bilateral.txt', 20);
 
-plot_surf('translate_scaled_bilinear.txt', 20);
-plot_surf('translate_bilinear.txt', 20);
+plot_surf('translate_scaled_bilinear.txt', 10);
+plot_surf('translate_bilinear.txt', 10);
 
 %{
 ima=giw_load('image_a.oct');
